5571eec543018138533fb18da62a240f8ecc77a0
shrinathprabhu <shrinathprabhu.123@gmail.com>
2025-08-27 18:20:46 +0530

    Update contract address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs'
 
 export const stakeContract = '0xCD492FeF558B9BF30B81e3706632b5394EF0e515'
-export const xarContract = '0xac03CEb03C3AE0dA5aA13c3490Bf4C1B2121b2b9'
-export const availContract = '0x7D0885d9b219D507195F2b63Fd95096e14346f16'
+export const xarContract = '0x5027Fc44a7Ba114B8f494B1e4970900C6652FEDF'
+export const availContract = '0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8'
 
 export const XARToAvailDivisor = 4
 
