75e59d756f0fd38e2d282ac14dbfe83708153851
Svarcoder <charvai1997@gmail.com>
2025-08-27 19:33:01 +0530

    Fix: Stake Contract Update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 
-export const stakeContract = '0xCD492FeF558B9BF30B81e3706632b5394EF0e515'
+export const stakeContract = '0x384eCF4a80a71bB4aAe77aE287DC78Dc100c58E4'
 export const xarContract = '0x5027Fc44a7Ba114B8f494B1e4970900C6652FEDF'
 export const availContract = '0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8'
 
