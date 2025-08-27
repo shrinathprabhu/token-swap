61ba52ddc7c81d33730c05a88ccc70d533a448a0
Svarcoder <charvai1997@gmail.com>
2025-08-27 19:43:55 +0530

    Fix: Update Time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,6 +12,6 @@ export function getTransactionUrl(txHash: string) {
   return new URL(`/tx/${txHash}`, explorerUrl).href
 }
 
-export const deadlineTime = dayjs('2026-02-08T00:00:00.000Z')
+export const deadlineTime = dayjs('2026-02-27T00:00:00.000Z')
 export const unlock1Time = dayjs('2026-02-28T00:00:00.000Z')
 export const unlock2Time = dayjs('2026-08-28T00:00:00.000Z')
